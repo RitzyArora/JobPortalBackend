6ec7e19dd550ebef993ff5a03acc97f15738d17c
Ritika Arora <ritikaaroramcageu@gmail.com>
2026-06-18 16:10:52 +0530

    Fixed middleware orders

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(cors({
     origin:"https://job-portal-frontend-ten-ruddy.vercel.app"
 }))
 app.use(express.json())
-app.use(errorHandler)
+
 app.use("/api/auth",authRoutes)
 app.use("/api/jobs",jobRoutes)
 app.use(morgan("dev"))
 
-
+app.use(errorHandler)
 module.exports=app
